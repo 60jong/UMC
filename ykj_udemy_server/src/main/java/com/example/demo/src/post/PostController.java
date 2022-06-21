10c2d8d6dd258c904e42aea2b6b6e2411205f4cb
KyeonJong-Yoo <rudwhd515@gmail.com>
2022-06-21 19:04:07 +0900

    initial commit

---
+++ ykj_udemy_server/src/main/java/com/example/demo/src/post/PostController.java
@@ -0,0 +1,126 @@
+package com.example.demo.src.post;
+
+import com.example.demo.config.BaseException;
+import com.example.demo.config.BaseResponse;
+import com.example.demo.src.user.model.*;
+import com.example.demo.utils.JwtService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import static com.example.demo.config.BaseResponseStatus.*;
+import static com.example.demo.utils.ValidationRegex.isRegexEmail;
+
+@RestController
+@RequestMapping("/users")
+public class PostController {
+    final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private final PostProvider postProvider;
+    @Autowired
+    private final PostService postService;
+    @Autowired
+    private final JwtService jwtService;
+
+
+
+
+    public PostController(PostProvider postProvider, PostService postService, JwtService jwtService){
+        this.postProvider = postProvider;
+        this.postService = postService;
+        this.jwtService = jwtService;
+    }
+
+
+    @ResponseBody
+    @GetMapping("/{userIdx}") // (GET) 127.0.0.1:9000/users/:userIdx
+    public BaseResponse<GetUserRes> getUserByIdx(@PathVariable("userIdx") int userIdx) throws BaseException {
+        if(postProvider.checkUser(userIdx) == 0)
+            throw new BaseException(USERS_EMPTY_USER_ID);
+        try{
+            GetUserRes getUsersRes = postProvider.getUserByIdx(userIdx);
+            return new BaseResponse<>(getUsersRes);
+        } catch(BaseException exception){
+            return new BaseResponse<>((exception.getStatus()));
+        }
+    }
+
+    @ResponseBody
+    @GetMapping("/{userIdx}/info") // (GET) 127.0.0.1:9000/users/:userIdx
+    public BaseResponse<GetUserInfoRes> getUserInfoByIdx(@PathVariable("userIdx") int userIdx) throws BaseException {
+        if(postProvider.checkUser(userIdx) == 0)
+            throw new BaseException(USERS_EMPTY_USER_ID);
+        try {
+            GetUserInfoRes getUserInfoRes = postProvider.getUsersInfoByIdx(userIdx);
+            return new BaseResponse<>(getUserInfoRes);
+        } catch (BaseException exception) {
+            return new BaseResponse<>((exception.getStatus()));
+        }
+
+    }
+
+    @ResponseBody
+    @GetMapping("/{userIdx}/posts")
+    public BaseResponse<GetUserFeedsRes> getUserFeedsByIdx(@PathVariable("userIdx") int userIdx) throws BaseException {
+        if(postProvider.checkUser(userIdx) == 0)
+            throw new BaseException(USERS_EMPTY_USER_ID);
+        GetUserFeedsRes getUserFeedsRes = postProvider.retrieveUserFeeds(userIdx,userIdx);
+
+        return new BaseResponse<>(getUserFeedsRes);
+    }
+    /**
+     * 회원가입 API
+     * [POST] /users
+     * @return BaseResponse<PostUserRes>
+     */
+    // Body
+    @ResponseBody
+    @PostMapping("") // (POST) 127.0.0.1:9000/users
+    public BaseResponse<PostUserRes> createUser(@RequestBody PostUserReq postUserReq) {
+        // TODO: email 관련한 짧은 validation 예시입니다. 그 외 더 부가적으로 추가해주세요!
+        if(postUserReq.getEmail() == null){
+            return new BaseResponse<>(POST_USERS_EMPTY_EMAIL);
+        }
+        // 이메일 정규표현
+        if(!isRegexEmail(postUserReq.getEmail())){
+            return new BaseResponse<>(POST_USERS_INVALID_EMAIL);
+        }
+        try{
+            PostUserRes postUserRes = postService.createUser(postUserReq);
+            return new BaseResponse<>(postUserRes);
+        } catch(BaseException exception){
+            return new BaseResponse<>((exception.getStatus()));
+        }
+    }
+
+    /**
+     * 유저정보변경 API
+     * [PATCH] /users/:userIdx
+     * @return BaseResponse<String>
+     */
+    @ResponseBody
+    @PatchMapping("/{userIdx}") // (PATCH) 127.0.0.1:9000/users/:userIdx
+    public BaseResponse<String> modifyUserName(@PathVariable("userIdx") int userIdx, @RequestBody User user){
+        try {
+            /* TODO: jwt는 다음주차에서 배울 내용입니다!
+            jwt에서 idx 추출.
+            int userIdxByJwt = jwtService.getUserIdx();
+            userIdx와 접근한 유저가 같은지 확인
+            if(userIdx != userIdxByJwt){
+                return new BaseResponse<>(INVALID_USER_JWT);
+            }
+            */
+
+            PatchUserReq patchUserReq = new PatchUserReq(userIdx,user.getNickName());
+            postService.modifyUserName(patchUserReq);
+
+            String result = "";
+        return new BaseResponse<>(result);
+        } catch (BaseException exception) {
+            return new BaseResponse<>((exception.getStatus()));
+        }
+    }
+
+}
